b9cd7741965261063125e4d1920ddbbfa31e617a
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-03-10 14:00:37 +0900

    Alphabets: random position

--- App.js
+++ App.js
@@ -1,33 +1,48 @@
 import React, { useState, useEffect } from "react";
+import { Animated, View, Dimensions } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import icons from "./icons";
 
 const MAIN_COLOR = "#CE0C5C";
 const ICONS_LENGTH = icons.length;
+const ALPHABET_BALL_SIZE = "40";
+
+const QUIZ_CONTAINER_FLEX = 2;
+const WORDS_CONTAINER_FLEX = 5;
+const ANSWER_CONTAINER_FLEX = 1;
+const ALL_CONTAINER_FLEX =
+  QUIZ_CONTAINER_FLEX + WORDS_CONTAINER_FLEX + ANSWER_CONTAINER_FLEX;
+
+const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get("window");
 
 // max exclusive: should be ICONS_LENGTH + 1
-const getRandomIconIndex = () => Math.floor(Math.random() * (ICONS_LENGTH + 1));
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
+const getRandomPosition = (maxX, maxY) => {
+  const x = getRandomNumber(maxX);
+  const y = getRandomNumber(maxY);
+  return { x, y };
+};
 const Container = styled.View`
   background-color: ${MAIN_COLOR};
   flex: 1;
 `;
-const IconContainer = styled.View`
-  flex: 3;
+const QuizIconContainer = styled.View`
+  flex: ${QUIZ_CONTAINER_FLEX};
   justify-content: flex-end;
   align-items: center;
 `;
 const WordsContainer = styled.View`
-  flex: 5;
+  flex: ${WORDS_CONTAINER_FLEX};
 `;
 const AnswerContainer = styled.View`
-  flex: 1;
+  flex: ${ANSWER_CONTAINER_FLEX};
   justify-content: center;
   align-items: center;
   margin-bottom: 20px;
 `;
-const Box = styled.Text`
+const AnswerBox = styled.Text`
   border: 1px solid white;
   color: white;
   width: auto;
@@ -40,15 +55,15 @@ const Box = styled.Text`
   border-radius: 10px;
   padding: 0 15px;
 `;
-const AlphabetContainer = styled.View`
+const AlphabetBall = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
-  width: 40px;
-  height: 40px;
+  width: ${ALPHABET_BALL_SIZE}px;
+  height: ${ALPHABET_BALL_SIZE}px;
   border-radius: 50px;
   justify-content: center;
   align-items: center;
 `;
-const Alphabet = styled.Text`
+const AlphabetText = styled.Text`
   color: ${MAIN_COLOR};
   font-size: 20px;
   line-height: 20px;
@@ -59,28 +74,42 @@ export default function App() {
   const [iconIndex, setIconIndex] = useState(0);
 
   const makeAlphabets = () => {
-    const arr = word.split("");
-    return arr.map((a, index) => (
-      <AlphabetContainer key={index}>
-        <Alphabet key={index}>{a}</Alphabet>
-      </AlphabetContainer>
-    ));
+    const wordArr = word.split("");
+    return wordArr.map((alphabet, index) => {
+      const position = getRandomPosition(
+        WINDOW_WIDTH - ALPHABET_BALL_SIZE,
+        (WINDOW_HEIGHT / ALL_CONTAINER_FLEX) * WORDS_CONTAINER_FLEX -
+          ALPHABET_BALL_SIZE
+      );
+      return (
+        <AlphabetBall
+          key={index}
+          style={{
+            position: "absolute",
+            top: position.y,
+            left: position.x,
+          }}
+        >
+          <AlphabetText key={index}>{alphabet}</AlphabetText>
+        </AlphabetBall>
+      );
+    });
   };
 
   useEffect(() => {
-    const randomIndex = getRandomIconIndex();
+    const randomIndex = getRandomNumber(0, ICONS_LENGTH + 1);
     setIconIndex(randomIndex);
     setWord(icons[randomIndex]);
   }, []);
 
   return (
     <Container>
-      <IconContainer>
+      <QuizIconContainer>
         <Ionicons name={icons[iconIndex]} size={90} color="white" />
-      </IconContainer>
+      </QuizIconContainer>
       <WordsContainer>{makeAlphabets()}</WordsContainer>
       <AnswerContainer>
-        <Box>BLABLA_</Box>
+        <AnswerBox>BLABLA_</AnswerBox>
       </AnswerContainer>
     </Container>
   );
