cc621d23ed33905394ccdef10847d0de6a92b171
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-03-17 23:34:07 +0900

    onPress function deleted, moved into PanResponder...

--- App.js
+++ App.js
@@ -43,7 +43,6 @@ const QuizIconContainer = styled.View`
 const WordsContainer = styled.View`
   flex: ${WORDS_CONTAINER_FLEX};
 `;
-
 const AlphabetBall = styled(Animated.createAnimatedComponent(View))`
   position: absolute;
   width: ${ALPHABET_BALL_SIZE}px;
@@ -51,6 +50,7 @@ const AlphabetBall = styled(Animated.createAnimatedComponent(View))`
   border-radius: 50px;
   justify-content: center;
   align-items: center;
+  background-color: white;
 `;
 const TouchableAlphabet = styled(Animated.createAnimatedComponent(Pressable))`
   position: absolute;
@@ -61,14 +61,12 @@ const TouchableAlphabet = styled(Animated.createAnimatedComponent(Pressable))`
   align-items: center;
   background-color: white;
 `;
-
 const AlphabetText = styled.Text`
   color: ${MAIN_COLOR};
   font-size: 20px;
   line-height: 20px;
   font-weight: 700;
 `;
-
 const AnswerContainer = styled.View`
   flex: ${ANSWER_CONTAINER_FLEX};
   justify-content: center;
@@ -111,7 +109,7 @@ const AnswerText = styled(Animated.createAnimatedComponent(Text))`
   align-self: center;
 `;
 export default function App() {
-  const [data, setData] = useState([]);
+  const [balls, setBalls] = useState([]);
   const [loading, setLoading] = useState(true);
   const [word, setWord] = useState("");
   const [quizIconIndex, setQuizIconIndex] = useState(0);
@@ -122,88 +120,12 @@ export default function App() {
   const [hiddenBalls, setHiddenBalls] = useState([]);
 
   const onPressInPosition = useRef(new Animated.ValueXY()).current;
+
   const ballScale = useRef(new Animated.Value(1)).current;
   const answerBgOpacity = useRef(new Animated.Value(0)).current;
   const answerOpacity = useRef(new Animated.Value(1)).current;
   const answerBgWidth = useRef(new Animated.Value(60)).current;
   const answerBorderWidth = useRef(new Animated.Value(60)).current;
-
-  const balls = useMemo(
-    () =>
-      data?.map((obj) => {
-        const {
-          position: { x, y },
-        } = obj;
-        return {
-          ...obj,
-          animatedVal:
-            onPressInIndex === obj.index
-              ? onPressInPosition.setValue({ x, y })
-              : null,
-        };
-      }),
-    [data, onPressInIndex]
-  );
-
-  const panResponder = useMemo(
-    () =>
-      PanResponder.create({
-        onStartShouldSetPanResponder: (_, { dx, dy }) => {
-          // if dragged ? setPanResponder
-          return dx > 2 || dx < -2 || dy > 2 || dy < -2;
-        },
-        onMoveShouldSetPanResponder: () => true,
-        onPanResponderGrant: () => {
-          // 오프셋 설정 (현 위치 누적값으로 저장)
-          onPressInPosition.setOffset({
-            x: onPressInPosition.x._value,
-            y: onPressInPosition.y._value,
-          });
-        },
-        onPanResponderMove: (_, { dx, dy }) => {
-          onPressInPosition.setValue({
-            x: dx,
-            y: dy,
-          });
-          // Animated.Value 중지된 마지막 포지션 저장
-          onPressInPosition.stopAnimation((val) => {
-            balls[onPressInIndex].position.x = val.x;
-            balls[onPressInIndex].position.y = val.y;
-          });
-        },
-        onPanResponderRelease: () => {
-          // 오프셋 + 이동거리 기본값 사용 후, 오프셋을 0으로 재설정
-          onPressInPosition.flattenOffset();
-        },
-      }),
-    [balls]
-  );
-
-  const makeAlphabets = () => {
-    const arr = word.split("");
-    const words = arr.map((alphabet, index) => {
-      const position = getRandomPosition(
-        WINDOW_WIDTH,
-        (WINDOW_HEIGHT / ALL_CONTAINER_FLEX) * WORDS_CONTAINER_FLEX
-      );
-      const word = { index, position, alphabet };
-      return word;
-    });
-    setData(words);
-    // setLoading(false);
-  };
-  useEffect(() => {
-    if (loading) {
-      makeAlphabets();
-    }
-  }, [word, loading]);
-
-  useEffect(() => {
-    // const randomIndex = getRandomNumber(ICONS_LENGTH + 1);
-    const randomIndex = getRandomNumber(1);
-    setQuizIconIndex(randomIndex);
-    setWord(icons[randomIndex]);
-  }, []);
 
   const scaleOut = Animated.timing(ballScale, {
     toValue: 0,
@@ -236,7 +158,6 @@ export default function App() {
     duration: 200,
     useNativeDriver: true,
   });
-
   const moveBallToBottom = Animated.timing(onPressInPosition, {
     toValue: {
       x: WINDOW_WIDTH / 2 - ALPHABET_BALL_SIZE + 40,
@@ -245,35 +166,101 @@ export default function App() {
     duration: 10,
     useNativeDriver: true,
   });
-
   const contentFadeAnim = Animated.sequence([
     Animated.parallel([answerFadeOut, answerBgFadeIn]),
     Animated.parallel([answerFadeIn, answerBgFadeOut]),
   ]);
-
-  const correctIndexClickedAnim = Animated.sequence([
-    moveBallToBottom,
-    Animated.parallel([scaleOut, moveBallToCenter, contentFadeAnim]),
-  ]);
-  const handleCorrectIndexClick = (alphabet, index) => {
-    // 클릭된 알파벳 순서가 맞다면 계속 진행
-    correctIndexClickedAnim.start(() => {
-      setAnswer((prev) => prev + alphabet);
-      ballScale.setValue(1);
-      // 알파벳 볼 제거
-      setHiddenBalls([...hiddenBalls, index]);
-    });
-    setCorrectIndex((prev) => prev + 1);
-  };
-
   const wrongIndexClickedAnim = Animated.spring(onPressInPosition, {
     toValue: {
-      x: onPressInPosition.x._value + getRandomNumber(20),
-      y: onPressInPosition.y._value + getRandomNumber(20),
+      x: onPressInPosition.x._value + getRandomNumber(10),
+      y: onPressInPosition.y._value + getRandomNumber(10),
     },
     useNativeDriver: true,
     tension: 1000,
   });
+  const correctIndexClickedAnim = Animated.sequence([
+    moveBallToBottom,
+    Animated.parallel([scaleOut, moveBallToCenter, contentFadeAnim]),
+  ]);
+
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderGrant: (evt) => {
+          const index = evt._targetInst.memoizedProps.index;
+          setOnPressInIndex(index);
+          // 오프셋 설정 (현 위치 누적값으로 저장)
+          onPressInPosition.setOffset({
+            x: balls[index].position.x,
+            y: balls[index].position.y,
+          });
+        },
+        onPanResponderMove: (evt, { dx, dy }) => {
+          const index = evt._targetInst.memoizedProps.index;
+          onPressInPosition.setValue({
+            x: dx,
+            y: dy,
+          });
+          // Animated.Value 중지된 마지막 포지션 저장
+          if (dx > 2 || dx < -2 || dy > 2 || dy < -2) {
+            onPressInPosition.stopAnimation((val) => {
+              balls[index].position.x = val.x;
+              balls[index].position.y = val.y;
+            });
+          }
+        },
+        onPanResponderRelease: () => {},
+        onPanResponderEnd: (evt, { dx, dy }) => {
+          const index = evt._targetInst.memoizedProps.index;
+          setOnPressInIndex(null);
+          onPressInPosition.setValue({ x: 0, y: 0 });
+
+          if (dx < 2 && dx > -2 && dy < 2 && dy > -2) {
+            if (correctIndex === index) {
+              correctIndexClickedAnim.start();
+              // DOESN'T WORK
+
+              //   () => {
+              //   setAnswer((prev) => prev + alphabet);
+              //   ballScale.setValue(1);
+              //   // 알파벳 볼 제거
+              //   setHiddenBalls([...hiddenBalls, index]);
+              // });
+              // setCorrectIndex((prev) => prev + 1);
+            }
+          }
+        },
+      }),
+    [balls, onPressInIndex]
+  );
+
+  const makeAlphabets = () => {
+    const arr = word.split("");
+    const words = arr.map((alphabet, index) => {
+      const position = getRandomPosition(
+        WINDOW_WIDTH,
+        (WINDOW_HEIGHT / ALL_CONTAINER_FLEX) * WORDS_CONTAINER_FLEX
+      );
+      const word = { index, position, alphabet };
+      return word;
+    });
+    setBalls(words);
+    // setLoading(false);
+  };
+  useEffect(() => {
+    if (loading) {
+      makeAlphabets();
+    }
+  }, [word, loading]);
+
+  useEffect(() => {
+    // const randomIndex = getRandomNumber(ICONS_LENGTH + 1);
+    const randomIndex = getRandomNumber(1);
+    setQuizIconIndex(randomIndex);
+    setWord(icons[randomIndex]);
+  }, []);
 
   return (
     <Container>
@@ -292,6 +279,8 @@ export default function App() {
             !hiddenBalls.includes(ball.index) && (
               <AlphabetBall
                 {...panResponder.panHandlers}
+                index={ball.index}
+                alphabet={ball.alphabet}
                 key={ball.index}
                 style={{
                   left: ball.index !== onPressInIndex ? ball.position.x : 0,
@@ -306,22 +295,7 @@ export default function App() {
                       : null,
                 }}
               >
-                <TouchableAlphabet
-                  onLongPress={() => {}}
-                  onPressIn={() => setOnPressInIndex(ball.index)}
-                  onPress={() => {
-                    // 오답이라면 공 튕겨나간다
-                    if (ball.index !== correctIndex) {
-                      wrongIndexClickedAnim.start();
-                      return;
-                    }
-                    handleCorrectIndexClick(ball.alphabet, ball.index);
-                    // 다음 터치 눌리는 데 너무 오래 걸리면 ? 다음 인덱스 공 흔들흔들
-                  }}
-                  key={ball.index}
-                >
-                  <AlphabetText key={ball.index}>{ball.alphabet}</AlphabetText>
-                </TouchableAlphabet>
+                <AlphabetText key={ball.index}>{ball.alphabet}</AlphabetText>
               </AlphabetBall>
             )
         )}
